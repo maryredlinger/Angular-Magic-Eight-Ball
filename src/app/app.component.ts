0bb467ae88e21ad98e514582110cbc394408db68
Mary Redlinger <MaryRedlinger@Marys-MacBook-Pro.local>
2020-05-27 11:31:33 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-magic-eight-ball';
+  heyThere () {
+    window.alert('hey')
+  }
+
+  shakeBall() {
+    var num = Math.floor(Math.random() * 7)
+
+    if(num === 0){
+      window.alert("better luck next time")
+    }if(num === 1){
+      window.alert("thats a no")
+    }if(num === 2){
+      window.alert("yes yes yes")
+    }if(num === 3){
+      window.alert("COVID-19 says most likely")
+    }if(num === 4){
+      window.alert("ask again")
+    }if(num === 5){
+      window.alert("you should google that")
+    }if(num === 6){
+      window.alert("ummm sure?")
+    }if(num === 7){
+      window.alert("thats a definite maybe i think probably")
+    }
+  }
 }
